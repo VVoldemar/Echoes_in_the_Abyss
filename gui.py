7011fc56ebb2c54f91f914439ec179c4738fb4b8
Tiunov Vladimir <tiunov2008.va@gmail.com>
2025-02-21 14:39:46 +0300

    added exe and editor changes

--- gui.py
+++ gui.py
@@ -1,22 +1,28 @@
-import pygame, sys
+import pygame
+import sys
 from button import Button
 from config import BG_IMG, WIDTH, HEIGHT
 from editor import create_level
 from main import start_game
-from utils import load_image, terminate
+from utils import get_font, load_image, terminate
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Echoes in the Abyss")
 
-bg = pygame.transform.scale(load_image(BG_IMG), (WIDTH, HEIGHT)) # Загрузка и масштабирование фонового изображения
+# Загрузка и масштабирование фонового изображения
+bg = pygame.transform.scale(load_image(BG_IMG), (WIDTH, HEIGHT))
 
-def get_font(size):
-    return pygame.font.Font("fonts/font.ttf", size)
+
+
+
 
 def level_select():
-    levels = ['lvl1.txt', 'lvl2.txt']
+    levels = []
+    with open('./levels/lvllist.txt', 'r') as file:
+        for line in file:
+            levels.append(line.strip())
     level_buttons = []
     while True:
         mouse_pos = pygame.mouse.get_pos()
@@ -27,10 +33,10 @@ def level_select():
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(WIDTH//2, 260))
         screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
         for i in range(len(levels)):
-            level_buttons.append(Button(pos=(400+i*50, 350), 
-                            text_input=str(i + 1), font=get_font(75), base_color="#06A77D", hovering_color="White"))
-        button_back = Button(pos=(WIDTH//2, 660), 
-                            text_input="BACK", font=get_font(75), base_color="#06A77D", hovering_color="White")
+            level_buttons.append(Button(pos=(400+i*50, 350),
+                                        text_input=str(i + 1), font=get_font(75), base_color="#06A77D", hovering_color="White"))
+        button_back = Button(pos=(WIDTH//2, 660),
+                             text_input="BACK", font=get_font(75), base_color="#06A77D", hovering_color="White")
 
         button_back.changeColor(mouse_pos)
         button_back.update(screen)
@@ -57,14 +63,15 @@ def game_over(win, collected_coins=0, total_coins=0):
 
         screen.blit(bg, (0, 0))
         if win:
-            text = get_font(45).render(f"Level completed. {collected_coins} / {total_coins} coins", True, "#06A77D")
+            text = get_font(45).render(
+                f"Level completed. {collected_coins} / {total_coins} coins", True, "#06A77D")
             text_rect = text.get_rect(center=(WIDTH//2, 260))
         else:
             text = get_font(45).render("Game over.", True, "#06A77D")
             text_rect = text.get_rect(center=(WIDTH//2, 260))
         screen.blit(text, text_rect)
-        back_to_levels = Button(pos=(WIDTH//2, 460), 
-                            text_input="BACK TO LEVELS", font=get_font(75), base_color="#06A77D", hovering_color="White")
+        back_to_levels = Button(pos=(WIDTH//2, 460),
+                                text_input="BACK TO LEVELS", font=get_font(75), base_color="#06A77D", hovering_color="White")
 
         back_to_levels.changeColor(mouse_pos)
         back_to_levels.update(screen)
@@ -76,6 +83,8 @@ def game_over(win, collected_coins=0, total_coins=0):
                     main_menu()
 
         pygame.display.update()
+
+
 def main_menu():
     while True:
         screen.blit(bg, (0, 0))
@@ -85,19 +94,19 @@ def main_menu():
         menu_text = get_font(100).render("MAIN MENU", True, "#b68f40")
         menu_rect = menu_text.get_rect(center=(WIDTH//2, 100))
 
-        play_button = Button(pos=(WIDTH//2, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#06A77D", hovering_color="White")
-        editor_button = Button(pos=(WIDTH//2, 400), 
-                            text_input="EDITOR", font=get_font(75), base_color="#06A77D", hovering_color="White")
-        quit_button = Button(pos=(WIDTH//2, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#06A77D", hovering_color="White")
+        play_button = Button(pos=(WIDTH//2, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#06A77D", hovering_color="White")
+        editor_button = Button(pos=(WIDTH//2, 400),
+                               text_input="EDITOR", font=get_font(75), base_color="#06A77D", hovering_color="White")
+        quit_button = Button(pos=(WIDTH//2, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#06A77D", hovering_color="White")
 
         screen.blit(menu_text, menu_rect)
 
         for button in [play_button, editor_button, quit_button]:
             button.changeColor(mouse_pos)
             button.update(screen)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
